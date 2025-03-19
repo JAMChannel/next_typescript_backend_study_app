bc90977611d9971f7549dd0ffbec8589c9cc9857
jam <pon244tt@gmail.com>
2025-03-19 15:41:09 +0900

    feat: Link使い分けの学習

--- src/app/rcc/page.tsx
+++ src/app/rcc/page.tsx
@@ -1,10 +1,15 @@
 'use client' // This is a directive to use client-side rendering
 
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function ClientComponent() {
+  const router = useRouter()
   console.log('ClientComponentが実行されました')
   return (
-    <div>Client</div>
+    <div>
+      Client
+      <button onClick={() => router.push('/about')}>About</button>
+    </div>
   )
 }
--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ClientComponent from '@/components/ClientComponent'
+import Link from 'next/link'
+
 export default function ServerComponent() {
   console.log("server componentが実行されました")
   return (
     <div>
       Server
       <ClientComponent />
+      <Link href="/about">About</Link>
     </div>
   )
 }
